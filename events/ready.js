92e9abab4094c7d0957f3b458aa8b4ba1da589d3
Andre Jarrell <mail@andrejarrell.com>
2021-03-12 23:29:00 +1000

    updated bump role

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@ module.exports = {
         }, ms('10s'))
         setInterval(() => {
             let channel = bot.channels.cache.get(channels.bots)
-            channel.send(`<@801667138673967104> Time to bump! \`!d bump\``)
+            channel.send(`<@819899948559630348> Time to bump! \`!d bump\``)
         },  ms('2h'))
     }
 }
