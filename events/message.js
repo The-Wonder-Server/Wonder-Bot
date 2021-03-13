30fd8fa5eb07bf7db93c13f4798daf19ad45ad95
Andre Jarrell <mail@andrejarrell.com>
2021-03-14 09:17:15 +1000

    removed cron bump now on timeout

--- events/message.js
+++ events/message.js
@@ -1,9 +1,22 @@
+let ms = require('ms')
+let { channels } = require('../config.json')
+
 let prefix = process.env.PREFIX
+let startBumpTimeout = bot => {
+    global.bumpTimeout = setTimeout(() => {
+        let channel = bot.channels.cache.get(channels.bots)
+        channel.send(`<@&819899948559630348> Time to bump! \`!d bump\``)
+    }, ms('2h'))
+}
 
 module.exports = {
     name: 'message',
     run: (bot, message) => {
         if (message.author.bot) return
+        if (message.content === '!d bump') {
+            clearTimeout(global.bumpTimeout)
+            startBumpTimeout(bot)
+        }
         if (!message.content.startsWith(prefix)) return
         let args = message.content.slice(prefix.length).split(/ +/)
         let commandName = args.shift().toLowerCase()
--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,13 @@
-let { CronJob } = require('cron')
+let ms = require('ms')
 let { guild, channels } = require('../config.json')
 
 module.exports = {
     name: 'ready',
     run: bot => {
         console.log(`Logged in as ${bot.user.tag}!`)
-        new CronJob('* * * * *', () => {
+        setInterval(() => {
             let { memberCount } = bot.guilds.cache.get(guild)
             bot.user.setActivity(`${memberCount} Members`, { type: 'WATCHING' })
-        }).start()
-        new CronJob('0 */2 * * *', () => {
-            let channel = bot.channels.cache.get(channels.bots)
-            channel.send(`<@&819899948559630348> Time to bump! \`!d bump\``)
-        }).start()
+        }, ms('1m'))
     }
 }
