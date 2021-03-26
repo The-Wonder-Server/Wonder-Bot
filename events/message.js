65ff952399ced8b61328ec1e2867fe11f00f69c0
Andre Jarrell <mail@andrejarrell.com>
2021-03-26 20:43:58 +1000

    removed bump reminder

--- events/message.js
+++ events/message.js
@@ -2,22 +2,22 @@ let ms = require('ms')
 let channels = require('@settings/channels')
 
 let prefix = process.env.PREFIX
-let startBumpTimeout = bot => {
-    global.bumpTimeout = setTimeout(() => {
-        let channel = bot.channels.cache.get(channels.bots)
-        channel.send(`<@&819899948559630348> Time to bump! \`!d bump\``)
-    }, ms('2h'))
-}
+// let startBumpTimeout = bot => {
+//     global.bumpTimeout = setTimeout(() => {
+//         let channel = bot.channels.cache.get(channels.bots)
+//         channel.send(`<@&819899948559630348> Time to bump! \`!d bump\``)
+//     }, ms('2h'))
+// }
 
 module.exports = {
     name: 'message',
     run: (bot, message) => {
         if (message.author.bot) return
-        if (message.content === '!d bump') {
-            if (!bot.production) return
-            clearTimeout(global.bumpTimeout)
-            startBumpTimeout(bot)
-        }
+        // if (message.content === '!d bump') {
+        //     if (!bot.production) return
+        //     clearTimeout(global.bumpTimeout)
+        //     startBumpTimeout(bot)
+        // }
         if (!message.content.startsWith(prefix)) return
         let args = message.content.slice(prefix.length).split(/ +/)
         let commandName = args.shift().toLowerCase()
