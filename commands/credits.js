168c49c9b5e154105973a51705052861cf2c82ac
BennovanDorst <bennovandorst@gmail.com>
2022-08-01 02:30:47 +0200

    [💙] Added credits command

---
+++ commands/credits.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'credits',
+    run: ({ message }) => {
+        message.channel.send('Big thanks to https://andre.cx/ for creating this masterpiece. Time to let your awesome creation shine!')
+    }
+}
