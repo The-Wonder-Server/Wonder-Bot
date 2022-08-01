f0840896ebfc352633f17bd4c35e09befdde7a34
BennovanDorst <bennovandorst@gmail.com>
2022-08-01 02:17:39 +0200

    [👑] Update for TWS

--- events/ready.js
+++ events/ready.js
@@ -4,11 +4,6 @@ module.exports = {
     name: 'ready',
     run: async bot => {
         console.log(`Logged in as ${bot.user.tag}!`)
-        bot.guild = bot.guilds.cache.get(process.env.GUILD)
-        setInterval(() => {
-            let { memberCount } = bot.guild
-            bot.user.setActivity(`${memberCount} Members`, { type: 'WATCHING' })
-        }, ms('1m'))
         if (!bot.production) return
         bot.invites = await bot.guild.fetchInvites()
     }
